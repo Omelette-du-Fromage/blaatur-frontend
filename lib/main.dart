844b9c400048ef09b9ca58d5b3aac912d20e6911
Jonas Haneborg <jon.haneborg@gmail.com>
2021-02-25 14:24:02 +0100

    Started on input field and go button

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title:'Flutter Web',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.amber,
-      ),
-      home: Scaffold(backgroundColor: Colors.blueGrey),
-    );
+        title: 'Flutter Web',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Colors.amber,
+        ),
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          body: Center(
+              child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Container(
+                  width: 300,
+                  child: new TextField(
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          filled: true,
+                          border: InputBorder.none,
+                          hintText: 'Starting point.'))),
+              new FlatButton(
+                child: new Text("GO"),
+                color: Colors.cyan,
+                onPressed: () {},
+              ),
+            ],
+          )),
+        ));
   }
 }
