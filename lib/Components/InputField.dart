ce8b0768fa3d22c824b2f3ebf0e10c6183bbfa51
Jonas Haneborg <jon.haneborg@gmail.com>
2021-02-28 14:39:12 +0100

    input field class start

--- lib/Components/InputField.dart
+++ lib/Components/InputField.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class InputField extends StatelessWidget {
   String input;
 
+  @override
   Widget build(BuildContext context) {
     return null;
   }
