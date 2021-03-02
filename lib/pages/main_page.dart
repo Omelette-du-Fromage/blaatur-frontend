0faeb56717f11483310aaea0a54d8972d0fcdeb3
Anonymous <nip009@uib.no>
2021-03-02 12:55:31 +0100

    Small cleanup

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:food_web/components/custom_slider.dart';
 import '../components/input_field.dart';
 import 'destination_page.dart';
 
 class MainPage extends StatelessWidget {
   // This widget is the root of your application.
-  InputField inputFieldStartingPoint = InputField("Starting point");
-  InputField inputFieldBudget = InputField("Budget");
+  final InputField inputFieldStartingPoint = InputField('Starting point');
+  final InputField inputFieldBudget = InputField('Budget');
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +19,7 @@ class MainPage extends StatelessWidget {
           inputFieldStartingPoint,
           inputFieldBudget,
           FlatButton(
-            child: Text("GO"),
+            child: Text('GO'),
             color: Colors.cyan,
             onPressed: () {
               Navigator.push(
@@ -28,7 +27,7 @@ class MainPage extends StatelessWidget {
                 MaterialPageRoute(
                   builder: (context) => DestinationRoute(
                     startLocation: inputFieldStartingPoint.value +
-                        "   " +
+                        '    ' +
                         inputFieldBudget.value,
                   ),
                 ),
