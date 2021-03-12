49d488b3a89c2c21347977af32043d058b256d52
Jonas Haneborg <jon.haneborg@gmail.com>
2021-03-12 12:28:07 +0100

    Updated design of main_page.

--- lib/components/input_field.dart
+++ lib/components/input_field.dart
@@ -10,12 +10,17 @@ class InputField extends StatelessWidget {
     return Container(
         width: 300,
         child: TextField(
+            style: TextStyle(color: Colors.white),
             onChanged: (val) => value = val,
             textAlign: TextAlign.center,
             decoration: InputDecoration(
-                fillColor: Colors.white,
+                enabledBorder: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                  borderSide: const BorderSide(color: Colors.grey, width: 0.0),
+                ),
+                hintStyle: TextStyle(color: Colors.white),
+                fillColor: Color.fromRGBO(1, 1, 1, 0.2),
                 filled: true,
-                border: InputBorder.none,
                 hintText: value)));
   }
 }
--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -10,30 +10,54 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      body: Center(
+        body: Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+        image: AssetImage('assets/images/pic2.jpg'),
+        fit: BoxFit.cover,
+      )),
+      child: Center(
           child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
+          SizedBox(height: 150),
+          Text(
+            'Lyst til å reise, men ikke vet hvor du vil dra?',
+            style: TextStyle(
+                color: Colors.white, fontSize: 50, fontFamily: 'Montserrat'),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Velg hvor du vil reise fra og trekk en tilfeldig tur!',
+            style: TextStyle(
+                color: Colors.white, fontSize: 30, fontFamily: 'Montserrat'),
+          ),
+          SizedBox(height: 200),
           inputFieldStartingPoint,
+          SizedBox(height: 10),
           inputFieldBudget,
-          FlatButton(
-            child: Text('GO'),
-            color: Colors.cyan,
+          SizedBox(height: 20),
+          RawMaterialButton(
             onPressed: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(
                   builder: (context) => DestinationRoute(
-                    startLocation: inputFieldStartingPoint.value
-                  ),
+                      startLocation: inputFieldStartingPoint.value),
                 ),
               );
             },
+            elevation: 2.0,
+            fillColor: Colors.white,
+            child: Icon(
+              Icons.public,
+              color: Colors.black,
+              size: 60.0,
+            ),
+            padding: EdgeInsets.all(2.0),
+            shape: CircleBorder(),
           ),
         ],
       )),
-    );
+    ));
   }
 }
