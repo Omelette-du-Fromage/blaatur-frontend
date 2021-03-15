08bd348415e223217e8ec298e48c976a4d7b5ccb
Tellfisk <tellev@outlook.com>
2021-03-15 13:11:13 +0100

    Main Page Widget extraction refactoring

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -10,54 +10,83 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      decoration: BoxDecoration(
+      body: Container(
+        decoration: BoxDecoration(
           image: DecorationImage(
-        image: AssetImage('assets/images/pic2.jpg'),
-        fit: BoxFit.cover,
-      )),
-      child: Center(
+            image: AssetImage('assets/images/pic2.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
           child: Column(
-        children: <Widget>[
-          SizedBox(height: 150),
-          Text(
-            'Lyst til å reise, men ikke vet hvor du vil dra?',
-            style: TextStyle(
-                color: Colors.white, fontSize: 50, fontFamily: 'Montserrat'),
+            children: <Widget>[
+              SizedBox(height: 150),
+              Text(
+                'Lyst til å reise, men ikke vet hvor du vil dra?',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                    fontFamily: 'Montserrat'),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Velg hvor du vil reise fra og trekk en tilfeldig tur!',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontFamily: 'Montserrat'),
+              ),
+              SizedBox(height: 200),
+              TravelForm(
+                  inputFieldStartingPoint: inputFieldStartingPoint,
+                  inputFieldBudget: inputFieldBudget)
+            ],
           ),
-          SizedBox(height: 10),
-          Text(
-            'Velg hvor du vil reise fra og trekk en tilfeldig tur!',
-            style: TextStyle(
-                color: Colors.white, fontSize: 30, fontFamily: 'Montserrat'),
+        ),
+      ),
+    );
+  }
+}
+
+class TravelForm extends StatelessWidget {
+  const TravelForm({
+    Key key,
+    @required this.inputFieldStartingPoint,
+    @required this.inputFieldBudget,
+  }) : super(key: key);
+
+  final InputField inputFieldStartingPoint;
+  final InputField inputFieldBudget;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        inputFieldStartingPoint,
+        SizedBox(height: 10),
+        inputFieldBudget,
+        SizedBox(height: 20),
+        RawMaterialButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DestinationRoute(
+                    startLocation: inputFieldStartingPoint.value),
+              ),
+            );
+          },
+          elevation: 2.0,
+          fillColor: Colors.white,
+          child: Icon(
+            Icons.public,
+            color: Colors.black,
+            size: 60.0,
           ),
-          SizedBox(height: 200),
-          inputFieldStartingPoint,
-          SizedBox(height: 10),
-          inputFieldBudget,
-          SizedBox(height: 20),
-          RawMaterialButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => DestinationRoute(
-                      startLocation: inputFieldStartingPoint.value),
-                ),
-              );
-            },
-            elevation: 2.0,
-            fillColor: Colors.white,
-            child: Icon(
-              Icons.public,
-              color: Colors.black,
-              size: 60.0,
-            ),
-            padding: EdgeInsets.all(2.0),
-            shape: CircleBorder(),
-          ),
-        ],
-      )),
-    ));
+          padding: EdgeInsets.all(2.0),
+          shape: CircleBorder(),
+        ),
+      ],
+    );
   }
 }
--- test/components/input_field_test.dart
+++ test/components/input_field_test.dart
@@ -1,9 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:food_web/components/input_field.dart';
 
 void main() {
+  testWidgets('InputField handles input correctly',
+      (WidgetTester tester) async {
+    InputField inputField = InputField('default');
+    await tester.pumpWidget(inputField);
+    await tester.enterText(find.byType(TextField), 'Bergen');
+    await tester.pump();
+    expect(defaultTextFinder, findsOneWidget);
+  });
+
   test(
-      'Given input Hint text When Inputfield.value is called Then return string Hint text',
+      'Given input Hint text When Inputfield.value Then return string Hint text',
       () async {
     final inputField = InputField('Hint text');
     expect(inputField.value, 'Hint text');
