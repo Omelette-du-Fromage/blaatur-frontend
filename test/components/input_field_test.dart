3c56ded88733c13658551c8b220e628ad2b0665f
JennStro <jennys1994@gmail.com>
2021-03-22 13:50:56 +0100

    Update tests

--- test/components/input_field_test.dart
+++ test/components/input_field_test.dart
@@ -5,7 +5,7 @@ import 'package:food_web/components/input_field.dart';
 void main() {
   testWidgets('InputField handles input correctly',
       (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(home: InputField('Starting point')));
+    await tester.pumpWidget(MaterialApp(home: InputField(hintText:'Starting point')));
     final textField = find.byType(TextField);
     await tester.enterText(textField, 'new text');
     final text = find.text('new text');
@@ -15,7 +15,7 @@ void main() {
   test(
       'Given input Hint text When Inputfield.value Then return string Starting point',
       () async {
-    final inputField = InputField('Starting point');
+    final inputField = InputField(hintText:'Starting point');
     expect(inputField.value, 'Starting point');
   });
 }
